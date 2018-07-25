f58b7262dfc65116e8c99e4affd77a8656a6a793
Dao Lam <daolam112@gmail.com>
2018-07-25 15:00:30 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
-prefix='export '; file=/tmp/buildEnv; newfile=/tmp/exportEnv; env > $file; 
+prefix='export '; file=/tmp/buildEnv; newfile=/tmp/exportEnv;
 while read -r line; do
 escapeQuote=`echo $line | sed 's/"/\\\"/g'`;
 newline=`echo $escapeQuote | sed 's/\([A-Z_][A-Z0-9_]*\)=\(.*\)/\1="\2"/'`;
 echo ${prefix}$newline;
 done < $file > $newfile;
-# mv $newfile $file
+mv $newfile $file
